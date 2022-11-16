9501dfada8c6e76d393c0902af0135ade8cd4609
idk <hankhill19580@gmail.com>
2022-11-16 18:02:34 -0500

    add some flags

--- tbget/main.go
+++ tbget/main.go
@@ -1,16 +1,36 @@
 package main
 
 import (
+	"flag"
 	"log"
+	"os"
 
 	tbget "i2pgit.org/idk/tb-downloader/helper"
 )
 
+func wd() string {
+	w, err := os.Getwd()
+	if err != nil {
+		return "."
+	}
+	return w
+}
+
+var (
+	launch    = flag.Bool("launch", false, "Launch Tor Browser after downloading")
+	directory = flag.String("directory", wd(), "Directory to download Tor Browser to")
+)
+
 func main() {
-	cmd, err := tbget.DownloadVerifyUnpackTorBrowser(".")
+	cmd, err := tbget.DownloadVerifyUnpackTorBrowser(*directory)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	out, err := cmd.CombinedOutput()
-	log.Println(out)
+	if *launch {
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println(out)
+	}
 }
